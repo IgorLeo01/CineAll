f91faf642aa25abc26fa3e59f671c264b6881c9c
igorleo01 <aionsong456@gmail.com>
2024-07-02 20:05:24 -0300

    .

---
+++ src/main/java/com/example/CineAll/CineAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.CineAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CineAll/CineAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CineAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
